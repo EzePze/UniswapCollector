16fb61148512543d176236dec67463655e11fbe9
Jay <taylorjay098@gmail.com>
2022-02-27 14:39:42 +1100

    documentation

--- uniswap_v3_collector.py
+++ uniswap_v3_collector.py
@@ -3,36 +3,34 @@ from web3 import Web3
 import asyncio
 import dotenv
 
-# add your blockchain connection information
+# Accesses the Infura ETH websocket node
 conf = dotenv.dotenv_values(".env")
 provider = Web3.WebsocketProvider(conf["INFURA_WS_ENDPOINT"])
 web3 = Web3(provider)
 
-# uniswap address and abi
+# Uniswap addresses
 uniswap_router = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 uniswap_factory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 uniswap_swaps = '0x7885e359a085372EbCF1ed6829402f149D02c600'
 uniswap_eth_usdc_pool = '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640'
 
+# Uniswap ABIs
 uniswap_router_abi = json.load(open("uniswap_router_abi.json"))
 uniswap_factory_abi = json.load(open("uniswap_factory_abi.json"))
 uniswap_swaps_abi = json.load(open("uniswap_swaps_abi.json"))
 uniswap_pool_abi = json.load(open("uniswap_pool_abi.json"))
 
+# Creating Contract objects for all the different smart contracts data will be pulled from
 factory_contract = web3.eth.contract(address=uniswap_factory, abi=uniswap_factory_abi)
 router_contract = web3.eth.contract(address=uniswap_router, abi=uniswap_router_abi)
 swaps_contract = web3.eth.contract(address=uniswap_swaps, abi=uniswap_swaps_abi)
 eth_usdc_pool_contract = web3.eth.contract(address=uniswap_eth_usdc_pool, abi=uniswap_pool_abi)
 
-# define function to handle events and print to the console
+# What to do when an event is triggered, currently just prints raw to console
 def handle_event(event):
     print(Web3.toJSON(event))
-    # and whatever
 
-
-# asynchronous defined function to loop
-# this loop sets up an event filter and is looking for new entires for the "PairCreated" event
-# this loop runs on a poll interval
+# Async loop process new events
 async def log_loop(event_filter, poll_interval):
     while True:
         for event in event_filter.get_new_entries():
@@ -40,26 +38,22 @@ async def log_loop(event_filter, poll_interval):
         await asyncio.sleep(poll_interval)
 
 
-# when main is called
-# create a filter for the latest block and look for the "PairCreated" event for the uniswap factory contract
-# run an async loop
-# try to run the log_loop function above every 2 seconds
+# Main function creates event listeners for all the different smart contracts and desired event, and sets up the async loop
 def main():
+
+    # From the factory, we want to know when a new pool is created
     factory_event_filter = factory_contract.events.PairCreated.createFilter(fromBlock='latest')
+    # From the pools themselves, we want to know when a new swap occurs
     eth_usdc_swap_event_filter = eth_usdc_pool_contract.events.Swap.createFilter(fromBlock='latest')
 
-    #block_filter = web3.eth.filter('latest')
-    # tx_filter = web3.eth.filter('pending')
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
             asyncio.gather(
-                log_loop(factory_event_filter, 2),
-                log_loop(eth_usdc_swap_event_filter, 2)
+                log_loop(factory_event_filter, 0.1),
+                log_loop(eth_usdc_swap_event_filter, 0.1)
             )
         )
-                # log_loop(block_filter, 2),
-                # log_loop(tx_filter, 2)))
     finally:
         # close loop to free up system resources
         loop.close()
